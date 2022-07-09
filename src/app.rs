c8f72d3ec4b8b75483b69ad821227da24696a2ca
Dennis Schimtzek <dms@denniss-air.lan>
2022-07-09 19:14:39 -0400

    fix background color

--- src/app.rs
+++ src/app.rs
@@ -43,12 +43,8 @@ impl eframe::App for TimesCircleApp {
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let my_frame = egui::containers::Frame {
-            inner_margin: egui::style::Margin { left: 10., right: 10., top: 10., bottom: 10. },
-            outer_margin: egui::style::Margin { left: 10., right: 10., top: 10., bottom: 10. },
-            rounding: egui::Rounding { nw: 1.0, ne: 1.0, sw: 1.0, se: 1.0 },
-            shadow: eframe::epaint::Shadow { extrusion: 1.0, color: Color32::YELLOW },
-            fill: Color32::LIGHT_BLUE,
-            stroke: egui::Stroke::new(2.0, Color32::GOLD),
+            fill: Color32::LIGHT_GRAY,
+            ..Default::default()
         };
         egui::CentralPanel::default().frame(my_frame).show(ctx, |ui| {
             egui::CollapsingHeader::new("Options").show(ui, |ui| {
